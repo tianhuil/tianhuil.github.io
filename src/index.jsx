9be084b8f8c0c7da65b35e642b0116c199bc8c5a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-06-19 11:00:54 -0400

    Remove last unused files

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import style from './style.css'
 import { NavbarInstance } from './navbar/navbar.jsx'
 import { Home } from './home/home.jsx'
 
