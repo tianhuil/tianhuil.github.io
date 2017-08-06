319726422b7a8d8a53cba2aa00bcfe0339056cb6
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-08-05 21:13:59 -0400

    Layout for companies

--- src/modules/companies.jsx
+++ src/modules/companies.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+const Company = ((props) => (
+  <div className="mb15 col-xs-6 col-sm-4">
+    <div className="logo-box">
+      <img className="logo-image" src={props.src}/>
+    </div>
+  </div>
+))
+
 const Companies = ({match}) => (
   <section>
-    <p>Companies</p>
+    <div className="row">
+      <Company src="/images/google.png" />
+      <Company src="/images/foursquare.png" />
+      <div className="clearfix visible-xs"></div>
+      <Company src="/images/a16z.png" />
+      <div className="clearfix visible-sm"></div>
+      <Company src="/images/bloomberg.png" />
+      <div className="clearfix visible-xs"></div>
+      <Company src="/images/deshaw.png" />
+      <Company src="/images/jpmorgan.png" />
+    </div>
   </section>
 )
 
