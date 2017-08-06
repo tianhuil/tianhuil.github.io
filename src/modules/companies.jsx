beb21904dfd874de21547f9e18eba65f972228c0
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-08-06 15:24:18 -0400

    links

--- src/modules/companies.jsx
+++ src/modules/companies.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Company = ((props) => (
   <div className={props.className ? props.className : "mt15 col-xs-6 col-sm-4"}>
-    <div className="logo-box">
-      <img delayed className="logo-image" src={props.src}/>
-    </div>
+    <a href={props.url} >
+      <div className="logo-box">
+        <img delayed className="logo-image" src={props.src}/>
+      </div>
+    </a>
   </div>
 ))
 
@@ -22,40 +24,46 @@ const Companies = ({match}) => (
     {/* Companies */}
     <Header title="Companies" subtitle="A few companies that I've worked for" />
     <div className="row">
-      <Company src="/images/google.png" />
-      <Company src="/images/foursquare.png" />
+      <Company src="/images/google.png" url="https://www.google.com" />
+      <Company src="/images/foursquare.png" url="https://foursquare.com" />
       <div className="clearfix visible-xs"></div>
-      <Company src="/images/a16z.png" />
+      <Company src="/images/a16z.png" url="https://a16z.com"/>
       <div className="clearfix visible-sm visible-md visible-lg"></div>
-      <Company src="/images/bloomberg.png" />
+      <Company src="/images/bloomberg.png" url="https://www.bloomberg.com"/>
       <div className="clearfix visible-xs"></div>
-      <Company src="/images/deshaw.png" />
-      <Company src="/images/jpmorgan.png" />
+      <Company src="/images/deshaw.png" url="https://deshaw.com" />
+      <Company src="/images/jpmorgan.png" url="https://www.jpmorgan.com" />
     </div>
 
     {/* Founded */}
     <Header title="Founded" subtitle="Company I founded" />
     <div className="row">
-      <Company src="/images/tdi.jpeg" className="mt15 col-xs-12 col-sm-6 col-sm-offset-3"/>
+      <Company src="/images/tdi.jpeg"
+          url="https://www.thedataincubator.com"
+          className="mt15 col-xs-12 col-sm-6 col-sm-offset-3"/>
     </div>
 
     {/* Universities */}
     <Header title="Universities" subtitle="Academic Institutions I've been affiliated with" />
     <div className="row">
-      <Company src="/images/princeton.svg" />
-      <Company src="/images/cambridge.jpg" />
+      <Company src="/images/princeton.svg" url="https://www.princeton.edu" />
+      <Company src="/images/cambridge.jpg" url="https://www.cam.ac.uk" />
       <div className="clearfix visible-xs"></div>
-      <Company src="/images/cornell.png" />
+      <Company src="/images/cornell.png" url="https://www.cornell.edu" />
     </div>
 
     {/* Awards */}
     <Header title="Awards" subtitle="Awards I have been fortunate enough to receive" />
     <div className="row">
-      <Company src="/images/marshall.gif" className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/intel-sts.png"  className="mt15 col-xs-6 col-sm-3" />
+      <Company src="/images/marshall.gif" url="http://www.marshallscholarship.org/"
+                className="mt15 col-xs-6 col-sm-3" />
+      <Company src="/images/intel-sts.png" url="https://student.societyforscience.org/regeneron-sts"
+                className="mt15 col-xs-6 col-sm-3" />
       <div className="clearfix visible-xs"></div>
-      <Company src="/images/hertz.jpg"  className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/nsf.png" className="mt15 col-xs-6 col-sm-3" />
+      <Company src="/images/hertz.jpg" url="http://hertzfoundation.org/default.aspx"
+                className="mt15 col-xs-6 col-sm-3" />
+      <Company src="/images/nsf.png" url="https://www.nsfgrfp.org/"
+                className="mt15 col-xs-6 col-sm-3" />
     </div>
   </section>
 )
