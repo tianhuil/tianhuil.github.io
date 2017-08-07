cb6e73c7a049049a097f9a1acedb124234f472c8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-08-06 20:10:24 -0400

    Refactor into sections

--- src/modules/professional.jsx
+++ src/modules/professional.jsx
@@ -11,76 +11,84 @@ const Company = ((props) => (
   </div>
 ))
 
-const Header = ((props) => (
-  <div className="row">
-    <div className="mt30 col-xs-12">
-      <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+const Section = ((props) => (
+  <section id={props.id}>
+    <div className="row">
+      <div className="mt30 col-xs-12">
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+      </div>
     </div>
-  </div>
+    {props.children}
+  </section>
 ))
 
 const Professional = ({match}) => (
-  <section>
+  <div>
     {/* Companies */}
-    <Header title="Companies" subtitle="Selected companies that for which I have worked" />
-    <div className="row">
-      <Company src="/images/google.png" url="https://www.google.com" />
-      <Company src="/images/foursquare.png" url="https://foursquare.com" />
-      <div className="clearfix visible-xs"></div>
-      <Company src="/images/a16z.png" url="https://a16z.com"/>
-      <div className="clearfix visible-sm visible-md visible-lg"></div>
-      <Company src="/images/bloomberg.png" url="https://www.bloomberg.com"/>
-      <div className="clearfix visible-xs"></div>
-      <Company src="/images/deshaw.png" url="https://deshaw.com" />
-      <Company src="/images/jpmorgan.png" url="https://www.jpmorgan.com" />
-    </div>
+    <Section id="companies" title="Companies" subtitle="Selected companies that for which I have worked">
+      <div className="row">
+        <Company src="/images/google.png" url="https://www.google.com" />
+        <Company src="/images/foursquare.png" url="https://foursquare.com" />
+        <div className="clearfix visible-xs"></div>
+        <Company src="/images/a16z.png" url="https://a16z.com"/>
+        <div className="clearfix visible-sm visible-md visible-lg"></div>
+        <Company src="/images/bloomberg.png" url="https://www.bloomberg.com"/>
+        <div className="clearfix visible-xs"></div>
+        <Company src="/images/deshaw.png" url="https://deshaw.com" />
+        <Company src="/images/jpmorgan.png" url="https://www.jpmorgan.com" />
+      </div>
+    </Section>
 
     {/* Founded */}
-    <Header title="Founded" subtitle="Company I founded" />
-    <div className="row">
-      <Company src="/images/tdi.jpeg"
-          url="https://www.thedataincubator.com"
-          className="mt15 col-xs-12 col-sm-6 col-sm-offset-3"/>
-    </div>
+    <Section id="founded" title="Founded" subtitle="Company I founded" >
+      <div className="row">
+        <Company src="/images/tdi.jpeg"
+            url="https://www.thedataincubator.com"
+            className="mt15 col-xs-12 col-sm-6 col-sm-offset-3"/>
+      </div>
+    </Section>
 
     {/* Universities */}
-    <Header title="Universities" subtitle="Academic Institutions where I have degrees or postdocs" />
-    <div className="row">
-      <Company src="/images/princeton.svg" url="https://www.princeton.edu" />
-      <Company src="/images/cambridge.jpg" url="https://www.cam.ac.uk" />
-      <div className="clearfix visible-xs"></div>
-      <Company src="/images/cornell.png" url="https://www.cornell.edu" />
-    </div>
+    <Section id="universities" title="Universities" subtitle="Academic Institutions where I have degrees or postdocs" >
+      <div className="row">
+        <Company src="/images/princeton.svg" url="https://www.princeton.edu" />
+        <Company src="/images/cambridge.jpg" url="https://www.cam.ac.uk" />
+        <div className="clearfix visible-xs"></div>
+        <Company src="/images/cornell.png" url="https://www.cornell.edu" />
+      </div>
+    </Section>
 
     {/* Awards */}
-    <Header title="Awards" subtitle="Selected awards I have been fortunate enough to receive" />
-    <div className="row">
-      <Company src="/images/marshall.gif" url="http://www.marshallscholarship.org/"
-                className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/intel-sts.png" url="https://student.societyforscience.org/regeneron-sts"
-                className="mt15 col-xs-6 col-sm-3" />
-      <div className="clearfix visible-xs"></div>
-      <Company src="/images/hertz.jpg" url="http://hertzfoundation.org/default.aspx"
-                className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/nsf.png" url="https://www.nsfgrfp.org/"
-                className="mt15 col-xs-6 col-sm-3" />
-    </div>
+    <Section id="awards" title="Awards" subtitle="Selected awards I have been fortunate enough to receive" >
+      <div className="row">
+        <Company src="/images/marshall.gif" url="http://www.marshallscholarship.org/"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <Company src="/images/intel-sts.png" url="https://student.societyforscience.org/regeneron-sts"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <div className="clearfix visible-xs"></div>
+        <Company src="/images/hertz.jpg" url="http://hertzfoundation.org/default.aspx"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <Company src="/images/nsf.png" url="https://www.nsfgrfp.org/"
+                  className="mt15 col-xs-6 col-sm-3" />
+      </div>
+    </Section>
 
     {/* Boards */}
-    <Header title="Boards" subtitle="Selected boards of which I'm a member" />
-    <div className="row">
-      <Company src="/images/marshall.jpg" url="https://www.marshallscholars.org/"
-                className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/any.jpg" url="https://www.americaneedsyou.org/"
-                className="mt15 col-xs-6 col-sm-3" />
-      <div className="clearfix visible-xs"></div>
-      <Company src="/images/lendit.png" url="http://www.lendit.com/"
-                className="mt15 col-xs-6 col-sm-3" />
-      <Company src="/images/strata.png" url="https://conferences.oreilly.com/strata"
-                className="mt15 col-xs-6 col-sm-3" />
-    </div>
-  </section>
+    <Section id="boards" title="Boards" subtitle="Selected boards of which I'm a member" >
+      <div className="row">
+        <Company src="/images/marshall.jpg" url="https://www.marshallscholars.org/"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <Company src="/images/any.jpg" url="https://www.americaneedsyou.org/"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <div className="clearfix visible-xs"></div>
+        <Company src="/images/lendit.png" url="http://www.lendit.com/"
+                  className="mt15 col-xs-6 col-sm-3" />
+        <Company src="/images/strata.png" url="https://conferences.oreilly.com/strata"
+                  className="mt15 col-xs-6 col-sm-3" />
+      </div>
+    </Section>
+  </div>
 )
 
 export default Professional
