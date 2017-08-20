10d9603a2942faa8dc1168d374086fc4339589e9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-08-20 14:29:25 -0400

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-# First run this command
+# First run this command:
 # git remote add deploy git@github.com:tianhuil/tianhuil.github.io.git
 
+dt=$(date)
+ts=$(date +%s)
+
+function echoblue {
+  echo -e "\x1B[1;34m${1}\x1B[0m"
+}
+
 git subtree push --prefix dist deploy master
+git tag -a "Deploy$ts" -m "Deploy on $dt"
+echoblue "Don't forget to git push origin Deploy$ts"
