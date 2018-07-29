61b9c30c875c80b7fa368c26d0dcd446bb910a75
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-07-29 11:48:21 -0400

    save output

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,6 @@ function echoblue {
   echo -e "\x1B[1;34m${1}\x1B[0m"
 }
 
-# build and export files to out directory
-# See https://github.com/zeit/next.js#usage
-npx next build
-npx next export -o out
-
 # deploy from out directory
 git push deploy `git subtree split --prefix out master`:master --force
 git tag -a "Deploy$ts" -m "Deploy on $dt"
