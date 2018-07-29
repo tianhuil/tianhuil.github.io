ae5a860a712cdf5b848548db570137fdfa660b75
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-07-29 11:28:10 -0400

    add next build and next export

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,12 @@ function echoblue {
   echo -e "\x1B[1;34m${1}\x1B[0m"
 }
 
+# build and export files to out directory
+# See https://github.com/zeit/next.js#usage
+npx next build
+npx next export -o out
+
+# deploy from out directory
 git push deploy `git subtree split --prefix out master`:master --force
 git tag -a "Deploy$ts" -m "Deploy on $dt"
 echoblue "Don't forget to git push origin Deploy$ts"
