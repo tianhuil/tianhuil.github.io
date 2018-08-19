09125b0feadc4ac437a31ab396af63b3792e0215
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-08-19 18:02:30 -0400

    Adding more to header

--- components/layout.jsx
+++ components/layout.jsx
@@ -72,34 +72,60 @@ const Header = () => (
   </div>
 )
 
+const HeadDiv = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Michael Li</title>
+
+    {/* Facebook */}
+    <meta property="og:title" content="Michael Li"/>
+    <meta property="og:image" content="/static/images/tianhuil.jpg"/>
+    <meta property="og:description" content="Personal Homepage for Tianhui Michael Li"/>
+    <meta property="og:url" content="https://tianhui.li"/>
+
+    {/* Twitter */}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@tianhuil" />
+    <meta name="twitter:title" content="Michael Li" />
+    <meta name="twitter:description" content="Personal Homepage for Tianhui Michael Li" />
+    <meta name="twitter:image" content="/static/images/tianhuil.jpg" />
+
+    {/* Stylesheet */}
+    <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossOrigin="anonymous"/>
+
+    {/* Favicons from https://www.favicon-generator.org/ */}
+    <link rel="apple-touch-icon" sizes="57x57" href="/static/favicons/apple-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="60x60" href="/static/favicons/apple-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/static/favicons/apple-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/static/favicons/apple-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/static/favicons/apple-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/static/favicons/apple-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/static/favicons/apple-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-icon-180x180.png" />
+    <link rel="icon" type="image/png" sizes="192x192"  href="/static/favicons/android-icon-192x192.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/static/favicons/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/static/favicons/manifest.json" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="msapplication-TileImage" content="/static/favicons/ms-icon-144x144.png" />
+    <meta name="theme-color" content="#ffffff" />
+  </Head>
+)
+
 class Layout extends React.Component {
   render() {
     return (
       <div>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
-          <meta charset="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Tianhui Michael Li</title>
-
-          {/* Facebook */}
-          <meta property="og:title" content="Michael Li"/>
-          <meta property="og:image" content="/images/tianhuil.jpg"/>
-          <meta property="og:description" content="Personal Homepage for Tianhui Michael Li"/>
-
-          {/* Twitter */}
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@tianhuil" />
-          <meta name="twitter:title" content="Michael Li" />
-          <meta name="twitter:description" content="Personal Homepage for Tianhui Michael Li" />
-          <meta name="twitter:image" content="https://tianhui.li/" />
-          <link rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-            crossorigin="anonymous"/>
-        </Head>
+        <HeadDiv />
         <Header />
         <section>
           <div className="container">
