163108e9ceef3b2dcf74031510c396fc086cf78f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-06-05 11:14:31 -0400

    Update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-const withPreact = require('next-plugin-preact')
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
 
-module.exports = withPreact({})
+module.exports = nextConfig
