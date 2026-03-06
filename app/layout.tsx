5dec487f5f6afbd8c40986648c49ce1f65dba9ae
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2026-03-06 18:01:52 -0500

    Save

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,88 +34,91 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/favicons/apple-icon-57x57.png"
+          rel='apple-touch-icon'
+          sizes='57x57'
+          href='/favicons/apple-icon-57x57.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/favicons/apple-icon-60x60.png"
+          rel='apple-touch-icon'
+          sizes='60x60'
+          href='/favicons/apple-icon-60x60.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/favicons/apple-icon-72x72.png"
+          rel='apple-touch-icon'
+          sizes='72x72'
+          href='/favicons/apple-icon-72x72.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/favicons/apple-icon-76x76.png"
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='/favicons/apple-icon-76x76.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/favicons/apple-icon-114x114.png"
+          rel='apple-touch-icon'
+          sizes='114x114'
+          href='/favicons/apple-icon-114x114.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/favicons/apple-icon-120x120.png"
+          rel='apple-touch-icon'
+          sizes='120x120'
+          href='/favicons/apple-icon-120x120.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicons/apple-icon-144x144.png"
+          rel='apple-touch-icon'
+          sizes='144x144'
+          href='/favicons/apple-icon-144x144.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/favicons/apple-icon-152x152.png"
+          rel='apple-touch-icon'
+          sizes='152x152'
+          href='/favicons/apple-icon-152x152.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-icon-180x180.png"
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-icon-180x180.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicons/android-icon-192x192.png"
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/favicons/android-icon-192x192.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicons/favicon-96x96.png"
+          rel='icon'
+          type='image/png'
+          sizes='96x96'
+          href='/favicons/favicon-96x96.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
         />
-        <link rel="manifest" href="/favicons/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel='manifest' href='/favicons/manifest.json' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta
+          name='msapplication-TileImage'
+          content='/favicons/ms-icon-144x144.png'
+        />
+        <meta name='theme-color' content='#ffffff' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
       </head>
-      <body className="min-h-screen">
+      <body className='min-h-screen'>
         <Navbar />
-        <main className="container mx-auto px-4">{children}</main>
+        <main className='container mx-auto px-4'>{children}</main>
       </body>
     </html>
   )
@@ -123,35 +126,35 @@ export default function RootLayout({
 
 function Navbar() {
   return (
-    <header className="border-b border-border py-1.5">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <Link 
-          href="/" 
-          className="text-4xl font-extralight text-primary hover:no-underline"
+    <header className='shadow-md mb-8'>
+      <div className='container px-4 py-4 mx-auto flex items-center justify-between'>
+        <Link
+          href='/'
+          className='text-4xl font-extralight text-primary hover:no-underline'
         >
           Michael Li
         </Link>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuLink 
-                href="/" 
+              <NavigationMenuLink
+                href='/'
                 className={navigationMenuTriggerStyle()}
               >
                 Home
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink 
-                href="/professional" 
+              <NavigationMenuLink
+                href='/professional'
                 className={navigationMenuTriggerStyle()}
               >
                 Professional
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink 
-                href="/writing" 
+              <NavigationMenuLink
+                href='/writing'
                 className={navigationMenuTriggerStyle()}
               >
                 Writing
